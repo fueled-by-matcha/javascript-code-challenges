14a714b5ef1ac166934036f8c65d648d3dde17b3
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-14 20:08:51 -0500

    Create justCoolStuff.js

---
+++ justCoolStuff.js
@@ -0,0 +1,19 @@
+// Write your code here:
+function justCoolStuff(arr1, arr2){
+  const same = arr1.filter(item => {
+    if(arr2.includes(item)){
+      return item;
+    }
+  });
+  return same;
+}
+
+// Feel free to uncomment the code below to test your function
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
+// Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+
