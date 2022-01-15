d10df7a70bb4a8d6e6d4b22ce08d8b8537899a72
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-14 19:59:11 -0500

    Create sortYears.js

---
+++ sortYears.js
@@ -0,0 +1,11 @@
+// Write your code here:
+function sortYears(years){
+  years.sort();
+  years.reverse();
+  return years;
+}
+
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+
+console.log(sortYears(years))
+// Should print [ 2018, 2011, 1999, 1982, 1970, 1963, 1951, 1922 ]
