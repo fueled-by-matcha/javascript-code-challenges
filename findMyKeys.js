145dcb95475115f34d04d3cc8faf834e4caab593
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-14 20:20:56 -0500

    Create findMyKeys.js

---
+++ findMyKeys.js
@@ -0,0 +1,12 @@
+// Write your code here:
+function findMyKeys(array){
+  let index = array.indexOf('keys');
+  return index;
+}
+
+// Feel free to comment out the code below to test your function
+
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff))
+// Should print 4
