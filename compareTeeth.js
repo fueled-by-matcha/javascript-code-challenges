30018faf5ec619e9964ba2f2ae78e8e50da5ebf3
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-14 20:18:33 -0500

    Create compareTeeth.js

---
+++ compareTeeth.js
@@ -0,0 +1,19 @@
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+// Write your code here:
+function sortSpeciesByTeeth(species){
+  species.sort(compareTeeth);
+  return species;
+}
+
+function compareTeeth(animal1, animal2){
+  return animal1.numTeeth > animal2.numTeeth;
+}
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
