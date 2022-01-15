fbd65742c5fdb03521a024b4d51ffe1f890d2838
Lauren <46908732+fueled-by-matcha@users.noreply.github.com>
2022-01-14 19:50:37 -0500

    Create squareNums.js

---
+++ squareNums.js
@@ -0,0 +1,13 @@
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+
+// Write your code here:
+function squareNums(numbers){
+  const squares = numbers.map(number => {
+    return number * number;
+  });
+  return squares;
+}
+
+console.log(squareNums(numbers));
